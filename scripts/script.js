d22608f6da04107596f610c5f35c4b6b08654e4a
KirstenvanOsch <90243604+KirstenvanOsch@users.noreply.github.com>
2021-09-16 08:47:22 +0200

    Add files via upload
    Bij deze voortgang ligt de focus heel erg op de header.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+var menuButton = document.querySelector("button"); 
+var navigatieMenu = document.querySelector("nav"); 
+
+function toonMenu() { 
+navigatieMenu.classlist.add(".navigatieMenu"); 
+   
+}
+
+
+menuButton.addEventListener("click", toonMenu); 
